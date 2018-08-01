0cfa074a901a1c270f49aa0b95ab0903237672d7
shadaileng <qpf0510@qq.com>
2018-08-01 17:29:01 +0800

    frame layout

--- src/components/frame.vue
+++ src/components/frame.vue
@@ -4,7 +4,7 @@
     <div class="content_frame">
       <div class="header_content_frame"></div>
       <div class="nav_content_frame"></div>
-      <div class="body_content_frame"></div>
+      <div class="body_content_frame">1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br></div>
     </div>
     <div class="footer_frame"></div>
   </div>
@@ -17,7 +17,13 @@ export default {
     return {}
   },
   methods: {},
-  mounted: function () {},
+  mounted: function () {
+    document.querySelector('.body_content_frame').addEventListener('scroll', (event) => {
+      console.log(event.target.scrollTop)
+      console.log(event.target.offsetHeight)
+      console.log(event.target.scrollHeight)
+    })
+  },
   destroyed: function () {}
 }
 </script>
@@ -26,67 +32,86 @@ export default {
 <style scoped>
 .panel_frame {
   background: #E3E7E2;
+  background-image: url('https://wx1.sinaimg.cn/mw1024/62d300b5gy1ftqx7oiqgvj20qo140gxf.jpg');
+  background-size: cover;
+  background-position: center;
   padding: 10px;
   overflow: hidden;
 }
-.panel_frame > div {
+.header_frame, .content_frame, .header_content_frame, .nav_content_frame, .body_content_frame, .footer_frame {
+  /*outline: 1px solid #f4f4f4;*/
+  border-radius: 5px;
+  background: #fffd;
+}
+.content_frame {
+  background: #fffa;
+}
+.panel_frame > * {
   margin: 0;
   padding: 0;
+}
+
+.body_content_frame::-webkit-scrollbar {
+  width: 5px;
+  height:10px;
+  background-color:#ffffff;
+}
+.body_content_frame::-webkit-scrollbar-track{
+  -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
+  border-radius: 10px;
+  background-color: #cecece;
+}
+.body_content_frame::-webkit-scrollbar-thumb{
+  border-radius: 10px;
+  -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,.3);
+  background-color:#b5b1b1;
 }
 .header_frame {
   width: 100%;
   height: 10%;
-  background: #fff;
-  border: 1px solid #f4f4f4;
-  margin-bottom: 3px;
+  margin-bottom: 1%;
   text-align: center;
   line-height: 80px;
 }
 .content_frame {
   width: 100%;
-  height: 75%;
-  margin-bottom: 5px;
+  height: 77%;
+  margin-bottom: 1%;
   font-size: 0px;
 }
 .header_content_frame {
   width: 100%;
   height: 5%;
-  background: #fff;
-  border: 1px solid #f4f4f4;
-  margin-bottom: 3px;
+  margin-bottom: 1%;
   text-align: center;
   line-height: 50px;
 }
 .nav_content_frame {
   width: 10%;
-  height: 89%;
+  height: 93%;
+  float: left;
   font-size: 16px;
   display: inline-block;
   align-items: top;
   text-align: center;
-  background: #fff;
-  border: 1px solid #f4f4f4;
   word-wrap: break-word;
 }
 .body_content_frame {
-  width: 85%;
-  height: 89%;
+  width: 89%;
+  height: 93%;
+  float: right;
   font-size: 16px;
   display: inline-block;
   vertical-align: top;
   text-align: center;
-  margin-left: 5px;
-  background: #eee;
-  border: 1px solid #f4f4f4;
+  /*margin-left: 1%;*/
   line-height: 100%;
+  overflow: auto;
 }
 .footer_frame {
   width: 100%;
   height: 10%;
-  background: #fff;
-  border: 1px solid #f4f4f4;
   text-align: center;
   line-height: 80px;
-  background: #ddd;
 }
 </style>
